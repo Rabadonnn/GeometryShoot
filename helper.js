ae27af53f54451c775c7a09ef479a51c69857133
Rabadonnn <solomonmihai10@gmail.com>
2019-08-04 21:55:44 +0300

    Day 2

--- helper.js
+++ helper.js
@@ -1,8 +1,40 @@
-
-
 const Helper = {
     distance: function (u, v) {
         return Math.sqrt(Math.pow(v.x - u.x, 2) + Math.pow(v.y - u.y, 2));
+    },
+
+    roundRect: function (ctx, x, y, width, height, radius, fill, stroke) {
+        if (typeof stroke == 'undefined') {
+            stroke = true;
+        }
+        if (typeof radius === 'undefined') {
+            radius = 5;
+        }
+        if (typeof radius === 'number') {
+            radius = { tl: radius, tr: radius, br: radius, bl: radius };
+        } else {
+            var defaultRadius = { tl: 0, tr: 0, br: 0, bl: 0 };
+            for (var side in defaultRadius) {
+                radius[side] = radius[side] || defaultRadius[side];
+            }
+        }
+        ctx.beginPath();
+        ctx.moveTo(x + radius.tl, y);
+        ctx.lineTo(x + width - radius.tr, y);
+        ctx.quadraticCurveTo(x + width, y, x + width, y + radius.tr);
+        ctx.lineTo(x + width, y + height - radius.br);
+        ctx.quadraticCurveTo(x + width, y + height, x + width - radius.br, y + height);
+        ctx.lineTo(x + radius.bl, y + height);
+        ctx.quadraticCurveTo(x, y + height, x, y + height - radius.bl);
+        ctx.lineTo(x, y + radius.tl);
+        ctx.quadraticCurveTo(x, y, x + radius.tl, y);
+        ctx.closePath();
+        if (fill) {
+            ctx.fill();
+        }
+        if (stroke) {
+            ctx.stroke();
+        }
     }
 }
 
@@ -69,7 +101,7 @@ class Rectangle {
     }
 
     center() {
-        return new Vector2(x + w / 2, y + h / 2);
+        return new Vector2(this.x + this.w / 2, this.y + this.h / 2);
     }
 
     top() {
@@ -87,10 +119,88 @@ class Rectangle {
     right() {
         return this.x + this.w;
     }
+
+    includes(v) {
+        if (v != null) {
+            return v.x > this.x && v.y > this.y && v.x < this.right() && v.y < this.bottom();
+        }
+        return false;
+    }
 }
-
-
 
 function RectFromPosition(pos, w, h) {
     return new Rectangle(pos.x - w / 2, pos.y - h / 2, w, h);
 }
+
+(function () {
+
+    var Camera = function (context, settings) {
+        settings = settings || {};
+        this.distance = 1000.0;
+        this.lookat = [0, 0];
+        this.context = context;
+        this.fieldOfView = settings.fieldOfView || Math.PI / 4.0;
+        this.viewport = {
+            left: 0,
+            right: 0,
+            top: 0,
+            bottom: 0,
+            width: 0,
+            height: 0,
+            scale: [1.0, 1.0]
+        };
+        this.updateViewport();
+    };
+
+    Camera.prototype = {
+        begin: function () {
+            this.context.save();
+            this.applyScale();
+            this.applyTranslation();
+        },
+        end: function () {
+            this.context.restore();
+        },
+        applyScale: function () {
+            this.context.scale(this.viewport.scale[0], this.viewport.scale[1]);
+        },
+        applyTranslation: function () {
+            this.context.translate(-this.viewport.left, -this.viewport.top);
+        },
+        updateViewport: function () {
+            this.aspectRatio = this.context.canvas.width / this.context.canvas.height;
+            this.viewport.width = this.distance * Math.tan(this.fieldOfView);
+            this.viewport.height = this.viewport.width / this.aspectRatio;
+            this.viewport.left = this.lookat[0] - (this.viewport.width / 2.0);
+            this.viewport.top = this.lookat[1] - (this.viewport.height / 2.0);
+            this.viewport.right = this.viewport.left + this.viewport.width;
+            this.viewport.bottom = this.viewport.top + this.viewport.height;
+            this.viewport.scale[0] = this.context.canvas.width / this.viewport.width;
+            this.viewport.scale[1] = this.context.canvas.height / this.viewport.height;
+        },
+        zoomTo: function (z) {
+            this.distance = z;
+            this.updateViewport();
+        },
+        moveTo: function (x, y) {
+            this.lookat[0] = x;
+            this.lookat[1] = y;
+            this.updateViewport();
+        },
+        screenToWorld: function (x, y, obj) {
+            obj = obj || {};
+            obj.x = (x / this.viewport.scale[0]) + this.viewport.left;
+            obj.y = (y / this.viewport.scale[1]) + this.viewport.top;
+            return obj;
+        },
+        worldToScreen: function (x, y, obj) {
+            obj = obj || {};
+            obj.x = (x - this.viewport.left) * (this.viewport.scale[0]);
+            obj.y = (y - this.viewport.top) * (this.viewport.scale[1]);
+            return obj;
+        }
+    };
+
+    this.Camera = Camera;
+
+}).call(this);
--- script.js
+++ script.js
@@ -1,20 +1,32 @@
+const mainMenuId = "menu";
+const gameScreenId = "game";
+const aboutScreenId = "about";
+
+var mouse;
+var mouseClicked;
+
 class Game {
     constructor() {
         this.width = 800;
         this.height = 600;
-        this.frameRate = 60;
-        this.now;
-        this.then = Date.now();
         this.delta;
-        this.interval = 1000 / this.frameRate;
+        this.lastUpdate = Date.now();
         this.canvas = document.createElement("canvas");
         document.body.appendChild(this.canvas);
         this.context = this.canvas.getContext("2d");
         this.canvas.width = this.width;
         this.canvas.height = this.height;
         this.backgroundColor = "rgb(191, 192, 255)";
-        this.currentScreenId = mainMenuId;
-        this.mouse;
+        this.currentScreenId = gameScreenId;
+        this.camera = new Camera(this.context);
+        this.c_updateFps = 0;
+        this.fps = 0;
+    }
+
+    tick() {
+        let now = Date.now();
+        this.delta = (now - this.lastUpdate) / 1000;
+        this.lastUpdate = now;
     }
 
     run() {
@@ -22,19 +34,36 @@ class Game {
             return this.run();
         });
 
-        this.now = Date.now();
-        this.delta = (this.now - this.then) / 1000;
+        this.tick();
 
-        if (this.delta > this.interval) {
-            this.then = this.now - (this.delta & this.interval)
-        }
+
         this.context.fillStyle = this.backgroundColor;
         this.context.fillRect(0, 0, this.width, this.height);
+
+        if (this.c_updateFps < 0) {
+            this.fps = Math.floor(1 / this.delta)
+            this.c_updateFps = 0.1;
+        } else {
+            this.c_updateFps -= this.delta;
+        }
+
+        this.context.fillStyle = textColor;
+        this.context.font = "15px Arial";
+        this.context.fillText("FPS: " + this.fps, 28, 15);
 
         switch (this.currentScreenId) {
             case mainMenuId:
                 MainMenuScreen.update();
                 MainMenuScreen.draw();
+                break;
+            case gameScreenId:
+                GameScreen.update();
+                GameScreen.draw();
+                break;
+            case aboutScreenId:
+                AboutScreen.update();
+                AboutScreen.draw();
+                break;
 
         }
 
@@ -49,6 +78,14 @@ class Game {
             } else {
                 this.canvas.height = this.height;
             }
+        });
+
+        window.addEventListener('mousedown', () => {
+            mouseClicked = true;
+        });
+
+        window.addEventListener('mouseup', () => {
+            mouseClicked = false;
         });
 
         window.onmousemove = this.mouseMove;
@@ -76,11 +113,42 @@ class Game {
     }
 }
 
-var mouse;
+const textColor = "rgb(39, 40, 68)";
 
 class Button {
-    constructor(pos, w, h) {
+    constructor(pos, w, h, text, onClick) {
         this.rectangle = RectFromPosition(pos, w, h);
+        this.text = text;
+        this.onClick = onClick;
+        this.textColor = textColor;
+        this.font = "60px Abel";
+    }
+
+    update() {
+        window.addEventListener('mouseup', () => {
+            if (this.rectangle.includes(mouse)) {
+                this.onClick();
+            }
+        });
+    }
+
+    draw() {
+        if (this.rectangle.includes(mouse)) {
+            game.context.strokeStyle = textColor;
+        } else {
+            game.context.strokeStyle = 'rgb(170, 170, 255)';
+        }
+
+
+
+        game.context.fillStyle = 'rgb(170, 170, 255)';
+        game.context.lineWidth = 2;
+        Helper.roundRect(game.context, this.rectangle.x, this.rectangle.y, this.rectangle.w, this.rectangle.h, 25, true, true);
+
+        game.context.fillStyle = this.textColor;
+        game.context.font = this.font;
+        game.context.textAlign = "center";
+        game.context.fillText(this.text, this.rectangle.center().x, this.rectangle.center().y + this.rectangle.w / 15);
     }
 }
 
@@ -104,47 +172,187 @@ function loadAssets() {
     }
 }
 
-var game;
+var game = new Game();
 
 loadAssets();
 
 window.onload = () => {
-    game = new Game();
+
+    MainMenuScreen.init();
+    AboutScreen.init();
+    GameScreen.init();
+
     game.run();
     console.log("Game started ...")
 }
 
-const mainMenuId = "menu";
-const gameScreenId = "game";
-const aboutScreenId = "about";
 
+class Player {
+    constructor() {
+        this.rectangle;
+        this.position = new Vector2(0, 0);
+        this.rotation = 0;
+        this.fuel = 100;
+        this.speed = 100;
+        this.direction = new Vector2(200, 200);
+        this.size = 50;
+
+        this.joystick = {
+            position: new Vector2(0, 0),
+            center: new Vector2(0, 0),
+            angle: 0,
+            hasValue: false,
+
+            update: function () {
+                if (mouseClicked) {
+                    if (!this.hasValue) {
+                        this.center = mouse;
+                        this.hasValue = true;
+                    }
+                    this.position = mouse;
+                } else {
+                    this.position = this.center;
+                }
+
+                let offset = new Vector2(0, 0);
+
+                if (this.hasValue && Helper.distance(this.center, this.position) > 15) {
+                    offset.x = this.position.x - this.center.x;
+                    offset.y = this.position.y - this.center.y;
+                } else {
+                    offset = new Vector2(0, 0);
+                }
+
+                this.angle = Math.atan2(offset.x, offset.y);
+
+                if (!mouseClicked) {
+                    this.hasValue = false;
+                }
+            }
+        }
+    }
+
+    init() {
+
+    }
+
+    update() {
+        this.joystick.update();
+        this.direction.mult(this.speed);
+        this.direction.mult(game.delta);
+        this.position.add(this.direction);
+        this.rectangle = RectFromPosition(this.position, this.size, this.size);
+        game.camera.moveTo(this.position.x, this.position.y);
+    }
+
+    draw() {
+        game.context.beginPath();
+        game.context.moveTo(this.joystick.center.x, this.joystick.center.y);
+        game.context.lineTo(this.joystick.position.x, this.joystick.position.y);
+        game.context.closePath();
+        game.context.stroke();
+
+        game.context.drawImage(assets['player'], this.rectangle.x, this.rectangle.y, this.rectangle.w, this.rectangle.h);
+    }
+}
+
+const player = new Player();
 
 var MainMenuScreen = {
-    update: function () {
+
+    playButton: new Button(new Vector2(game.width / 2, game.height / 2 + 75), 300, 100, "PLAY", () => {
+        game.currentScreenId = gameScreenId;
+    }),
+
+    aboutButton: new Button(new Vector2(game.width / 2, game.height / 2 + 200), 300, 100, "ABOUT", () => {
+        game.currentScreenId = aboutScreenId;
+    }),
+
+    init: function () {
 
     },
 
+    update: function () {
+        this.playButton.update();
+        this.aboutButton.update();
+    },
+
+    bigText: false,
+    bigTextCooldown: 0.25,
+    c_bigTextCooldown: 0,
+
     draw: function () {
 
+        if (this.c_bigTextCooldown < 0) {
+
+            if (this.bigText) {
+                this.bigText = false;
+            } else {
+                this.bigText = true;
+            }
+
+            this.c_bigTextCooldown = this.bigTextCooldown;
+        } else {
+            this.c_bigTextCooldown -= game.delta;
+        }
+
+        if (this.bigText) {
+            game.context.font = "125px Abel";
+        } else {
+            game.context.font = "120px Abel";
+        }
+
+        game.context.fillStyle = textColor;
+        game.context.fillText("PLANER", game.width / 2, 200);
+
+        this.playButton.draw();
+        this.aboutButton.draw();
     }
 };
 
 var GameScreen = {
-    update: function () {
+    init: function () {
+        player.init();
+    },
 
+    update: function () {
+        player.update();
     },
 
     draw: function () {
 
+        this.drawPlayerFuel();
+        game.camera.begin();
+        player.draw();
+        game.camera.end();
+    },
+
+    drawPlayerFuel: function () {
+        let w = mapValue(player.fuel, 100, 0, 600, 0);
+        game.context.fillStyle = textColor;
+        Helper.roundRect(game.context, 100, 30, w, 18, 10, true, false);
     }
 };
 
 var AboutScreen = {
-    update: function () {
 
+    backButton: new Button(new Vector2(game.width / 2, game.height - 100), 200, 50, "BACK", () => {
+        game.currentScreenId = mainMenuId;
+    }),
+
+    init: function () {
+        this.backButton.font = "40px Abel";
+    },
+
+    update: function () {
+        this.backButton.update();
     },
 
     draw: function () {
+        game.context.font = "120px Abel";
+        game.context.fillStyle = textColor;
+        game.context.fillText("ABOUT", game.width / 2, 200);
 
+        this.backButton.draw();
     }
 }
